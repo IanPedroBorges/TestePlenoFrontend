bdbacf978451d47215bdb29adf8100b5bfbe1c29
IanPedroBorges <ianhudback@gmail.com>
2025-04-26 03:20:40 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  setCount(1);
-  console.log(count);
   return (
     <>
       <div>
-        <h3>Teste Frontend PL </h3>
+        <h3>Teste aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa PL </h3>
       </div>
     </>
   )
